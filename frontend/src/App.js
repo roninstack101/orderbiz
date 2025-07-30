b0ddcd6d60022c4d3cc3a37436f02035f88aaff1
roninstack101 <yashurathore2001@gmail.com>
2025-07-30 14:03:12 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import './App.css';
+import Registration from './components/registration';
+import LoginPage from './components/login';
+import RegistrationPage from './components/login';
+
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Registration/> */}
+     {/* <LoginPage/> */}
+     <RegistrationPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/registration.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Registration = () => {
+
+    const [isShop, setisShop] = useState(false);
+    return (
+        <div className="min-h-screen flex">
+
+        <div className=""></div>
+        rgmrmrg
+        </div>
+    )
+};
+
+export default Registration;
